17a48240785c58d26b4f898419bcbd19756e4673
Vincent Chen <yinhsuac@uci.edu>
2026-08-13 04:02:55 -0700

    feat: add links in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,14 +9,21 @@ export default function Navbar() {
     <nav className="mx-4 px-4 py-2 border-b flex justify-between items-center">
       <h4>RoleBell</h4>
       {loading ? null : session ? (
-        <div className="flex items-center gap-3">
-          <span>{session.user.email}</span>
-          <button
-            className="self-stretch -my-2 px-4 bg-[#6b52a6] text-white hover:bg-[#5b2bca] cursor-pointer"
-            onClick={() => supabase.auth.signOut()}
-          >
-            Log Out
-          </button>
+        <div className="flex items-center gap-12">
+          <div className="flex items-center gap-12">
+            <Link to="/trending">Trending</Link>
+            <Link to="/my_tracker">My Tracker</Link>
+            <Link to="/create_tracker">Create Tracker</Link>
+          </div>
+          <div className="flex items-center gap-3">
+            <span>{session.user.email}</span>
+            <button
+              className="self-stretch -my-2 px-4 bg-[#6b52a6] text-white hover:bg-[#5b2bca] cursor-pointer"
+              onClick={() => supabase.auth.signOut()}
+            >
+              Log Out
+            </button>
+          </div>
         </div>
       ) : (
         <Link className="self-stretch -my-2 px-4 flex items-center bg-[#6b52a6] text-white hover:bg-[#5b2bca] cursor-pointer"
