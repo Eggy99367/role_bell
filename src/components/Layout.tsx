8d90216f6a238ffbcfd51615babdabe454597799
Vincent Chen <yinhsuac@uci.edu>
2026-08-13 13:09:19 -0700

    feat: add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,9 @@ export default function Layout() {
       <div className='mx-20 py-20 flex-1 flex flex-col'>
         <Outlet />
       </div>
+      <footer className='text-center text-xs text-gray-400 py-4'>
+        Made by <a href='https://github.com/Eggy99367' target='_blank' rel='noopener noreferrer' className='hover:underline'>Vincent Chen</a>
+      </footer>
     </div>
   )
 }
