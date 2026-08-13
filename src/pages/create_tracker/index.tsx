41e27359cc7d941edf15e43e5694bd4e713d1cd9
Vincent Chen <yinhsuac@uci.edu>
2026-08-13 05:54:08 -0700

    feat: add copy btn for manual checking on fetch result

--- src/pages/create_tracker/index.tsx
+++ src/pages/create_tracker/index.tsx
@@ -6,15 +6,28 @@ export default function CreateTracker() {
   const [loading, setLoading] = useState(false);
   const [verified, setVerified] = useState(false);
   const [failed, setFailed] = useState(false);
-  const [targetURL, setTargetURL] = useState("")
+  const [targetURL, setTargetURL] = useState("");
+  const [fetchResult, setFetchResult] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleVerify = async () => {
     setLoading(true);
-    const ok = await verifyWebContent(targetURL);
+    const { ok, content } = await verifyWebContent(targetURL);
     setVerified(ok);
     setFailed(!ok);
+    setFetchResult(content);
     setLoading(false);
   }
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(fetchResult);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
 
   return (
     <RequireAuth>
@@ -30,8 +43,10 @@ export default function CreateTracker() {
           disabled={loading || verified}
         />
         <button onClick={handleVerify} disabled={loading || verified}>Verify URL</button>
-        {failed && <p>fetch failed</p>}
-        {verified && <p>fetch successfully</p>}
+        {failed && <p>Fetch failed! The URL may be incorrect or the webpage could not be retrieved.</p>}
+        {verified && <p>Fetch successful! Please copy the fetched results into an <a href="https://html.onlineviewer.net/" target='blank'>Online HTML Viewer</a> to double-check that the output is correct.</p>}
+        <button onClick={handleCopy} disabled={!verified}>{isCopied ? "Copied" : "Copy Fetch Result"}</button>
+        
       </div>
     </RequireAuth>
   )
--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,15 +2,15 @@ import axios from "axios";
 
 const WORKER_URL = import.meta.env.VITE_WORKER_URL;
 
-export async function verifyWebContent(url: string): Promise<boolean> {
+export async function verifyWebContent(url: string): Promise<{ ok: boolean, content: string }> {
   try {
     const response = await axios.get(WORKER_URL, { params: { url } });
     console.log(response.data.content);
-    return response.data.ok === true;
+    return {ok: response.data.ok === true, content: response.data.content};
   } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(`URL verify error: ${err.message}`);
     }
-    return false;
+    return {ok: false, content: ""};
   }
 }
